ca1904c9dd8fb6c34f503a796aa68dfe244617b3
Nikola Perica <np46120@oss.unist.hr>
2024-03-01 13:02:57 +0100

    added city search
    added city search using jotai for global state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,9 @@ import WeatherDetails from "./components/WeatherDetails";
 import { metersToKilometers } from "./utils/metersToKilometers";
 import { convertWindSpeed } from "./utils/convertWindSpeed";
 import ForecastWeatherDetail from "./components/ForecastWeatherDetail";
+import { useAtom } from "jotai";
+import { placeAtom } from "./atom";
+import { useEffect } from "react";
 
 //https://api.openweathermap.org/data/2.5/forecast?q=Split&appid=7f179e6b525bf3a607c6e4f39b5a0e94&units=metric&cnt=56
 
@@ -71,12 +74,20 @@ interface WeatherData {
 
 
 export default function Home() {
-    const { isLoading, error, data } = useQuery<WeatherData>('repoData', async () =>{
+
+  const[place, setPlace]=useAtom(placeAtom)
+
+    const { isLoading, error, data, refetch } = useQuery<WeatherData>('repoData', async () =>{
       const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=Split&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&units=metric&cnt=56`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&units=metric&cnt=56`
       );
       return data;
     }
+    )
+
+    useEffect(()=>{
+      refetch()
+    },[place, refetch]
     )
 
     const firstData=data?.list[0]
@@ -107,7 +118,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-      <Navbar />
+      <Navbar location={data?.city.name}/>
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
         {/*today data */}
         <section className="space-y-4">
